db73c6276d60d308b1d0a2c9e8acc3bcdf0259c7
Dustin Boersma <dustin.boersma@gmail.com>
2015-05-01 19:05:46 -0700

    fixed chat height

--- app/assets/javascripts/components/twitchChat.jsx
+++ app/assets/javascripts/components/twitchChat.jsx
@@ -2,12 +2,31 @@ var React = require('react');
 
 var twitchChat = React.createClass({
 
+  componentDidUpdate: function() {
+    this.updateChatSize();
+  },
+
+  componentDidMount: function() {
+    this.updateChatSize();
+    window.addEventListener('resize', this.updateChatSize);
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener('resize', this.updateChatSize);
+  },
+
+  updateChatSize: function() {
+    var twitchChat = this.refs.twitchChat.getDOMNode();
+    var newHeight = window.innerHeight - twitchChat.getBoundingClientRect().top;
+    twitchChat.style.height = newHeight + 'px';
+  },
+
   render: function() {
 
     var chatUrl = this.props.stream.channel.url + '/chat?popout=';
 
     return (
-      <div className="stream-chat embed-container">
+      <div className="stream-chat embed-container" ref="twitchChat">
         <iframe src={chatUrl} frameborder="0" scrolling="no"></iframe>
       </div>
     );
