5f807be089c55ec16fdb60059006184b339a31f7
Dustin Boersma <dustin.boersma@gmail.com>
2018-12-15 23:08:36 -0800

    fixed issue with chat

--- app/assets/javascripts/components/twitchChat/twitchChat.jsx
+++ app/assets/javascripts/components/twitchChat/twitchChat.jsx
@@ -4,7 +4,7 @@ var twitchChat = React.createClass({
 
   render: function() {
 
-    var chatUrl = 'http://twitch.tv/' + this.props.stream.name + '/chat?popout=';
+    var chatUrl = 'http://twitch.tv/embed/' + this.props.stream.name + '/chat';
 
     return (
       <div className="stream-chat embed-container" ref="twitchChat">
